d6130202b02d1d60695e6e612621051fc4262686
whiteapolo <darkpendraw@proton.me>
2025-08-09 14:59:59 +0300

    expand tilde

--- source/app.d
+++ source/app.d
@@ -6,90 +6,94 @@ import std.typecons : tuple, Tuple;
 import std.regex;
 import core.stdc.stdlib;
 import std.exception;
+import std.path : expandTilde;
 
-struct DesktopFile {
-  string name;
-  string exec;
+struct DesktopFile
+{
+    string name;
+    string exec;
 }
 
 string[] desktopFileDirs = [
-  "/usr/share/applications",
-  "~/.local/share/applications",
-  "/var/lib/flatpak/exports/share/applications",
+    "/usr/share/applications", "~/.local/share/applications",
+    "/var/lib/flatpak/exports/share/applications",
 ];
-
-string expandPath(string path)
-{
-  if (path.startsWith("~")) {
-    return environment.get("HOME", "./") ~ path[1 .. $];
-  }
-
-  return path;
-}
 
 DesktopFile proccessDesktopFile(string pathname)
 {
-  string content = readText(pathname);
+    string content = readText(pathname);
 
-  auto execMatch = matchFirst(content, regex(`Exec=([^\n]*)`));
-  auto nameMatch = matchFirst(content, regex(`Name=([^\n]*)`));
+    auto execMatch = matchFirst(content, regex(`Exec=([^\n]*)`));
+    auto nameMatch = matchFirst(content, regex(`Name=([^\n]*)`));
 
-  enforce(nameMatch && execMatch, "name and exec was not found");
+    enforce(nameMatch && execMatch, "name and exec was not found");
 
-  auto name = nameMatch.captures[1].strip();
-  auto exec = execMatch.captures[1].replaceAll(regex(`%\w+`), "").strip();
+    auto name = nameMatch.captures[1].strip();
+    auto exec = execMatch.captures[1].replaceAll(regex(`%\w+`), "").strip();
 
-  return DesktopFile(name, exec);
+    return DesktopFile(name, exec);
 }
 
 void proccessDir(string pathname, ref string[string] programs)
 {
-  foreach (de; dirEntries(expandPath(pathname), SpanMode.shallow)) {
-    if (de.isFile && de.name.endsWith(".desktop")) {
-      try {
-        DesktopFile file = proccessDesktopFile(de.name);
-        programs[file.name] = file.exec;
-      } catch (Exception e) {}
+    foreach (de; dirEntries(expandTilde(pathname), SpanMode.shallow))
+    {
+        if (de.isFile && de.name.endsWith(".desktop"))
+        {
+            try
+            {
+                DesktopFile file = proccessDesktopFile(de.name);
+                programs[file.name] = file.exec;
+            }
+            catch (Exception e)
+            {
+            }
+        }
     }
-  }
 }
 
 string[string] proccessDirs(string[] dirs)
 {
-  string[string] programs;
+    string[string] programs;
 
-  foreach (dir; desktopFileDirs) {
-    proccessDir(dir, programs);
-  }
+    foreach (dir; desktopFileDirs)
+    {
+        proccessDir(dir, programs);
+    }
 
-  return programs;
+    return programs;
 }
 
 int main(string[] args)
 {
-  auto pipe = pipeProcess(["dmenu"] ~ args[1 .. $], Redirect.stdin | Redirect.stdout);
-  auto programs = proccessDirs(desktopFileDirs);
+    auto pipe = pipeProcess(["dmenu"] ~ args[1 .. $], Redirect.stdin | Redirect.stdout);
+    auto programs = proccessDirs(desktopFileDirs);
 
-  foreach (program; programs.keys) {
-    pipe.stdin.writeln(program);
-  }
+    foreach (program; programs.keys)
+    {
+        pipe.stdin.writeln(program);
+    }
 
-  pipe.stdin.close();
-  string selectedProgram = pipe.stdout.readln().strip();
-  pipe.stdout.close();
+    pipe.stdin.close();
+    string selectedProgram = pipe.stdout.readln().strip();
+    pipe.stdout.close();
 
-  if (selectedProgram.empty) {
-    return 1;
-  }
+    if (selectedProgram.empty)
+    {
+        return 1;
+    }
 
-  writeln("Selected program: ", selectedProgram);
+    writeln("Selected program: ", selectedProgram);
 
-  if (auto exec = programs.get(selectedProgram, null)) {
-    writeln("Running: '", exec, "'");
-    system(exec.toStringz());
-    return 0;
-  } else {
-    writeln("program doesn't exitst: ", selectedProgram);
-    return 2;
-  }
+    if (auto exec = programs.get(selectedProgram, null))
+    {
+        writeln("Running: '", exec, "'");
+        system(exec.toStringz());
+        return 0;
+    }
+    else
+    {
+        writeln("program doesn't exitst: ", selectedProgram);
+        return 2;
+    }
 }
